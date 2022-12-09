4d075afc90cc99d4b085d9e9e1ea5802aaef8cc6
Pietro De Domenico <pietrodd2002@gmail.com>
2022-12-09 20:40:43 +0100

    Initial commit

---
+++ Arkivr.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Help function
+function help_message () {
+    cat << EOF
+    Possible uses:
+    Arkivr --compress <directory> <-- Runs Arkivr in a given entrypoint
+    Arkivr --formats   <-- List formats that will be compressed
+    Arkivr --help      <-- Shows this help message
+EOF
+}
+
+# CONSTANTS
+multimedia_video=("m4v" "mov" "mp4")
+multimedia_images=("jpg")
+unstable_multimedia_images("png") # PNG to AVIF conversion using FFMPEG generates severe artifacts, and intermediate conversion to lossless JPG is required
+
+function supported_formats () {
+    cat << EOF
+    Supported formats:
+    Video files (Compressed using HandBrakeCLI / x265 CRF 25): ${multimedia_video[*]}
+    Image files (Compressed using ffmpeg / AVIF CRF 25): ${multimedia_images[*]}
+EOF
+}
+
+function compress () {
+    # ARGUMENTS
+    entrypoint=$1
+    pushd "$entrypoint"
+    tput init
+
+    # Count pictures
+    counter_pictures=0
+    for iterator in ${multimedia_images[@]}; do
+        pictures=$(find . -type f -name "*.$iterator" | wc -l)
+        counter_pictures=$(($counter_pictures + $pictures))
+    done
+    echo "Total pictures: $counter_pictures"
+    # Count videos
+    counter_videos=0
+    for iterator in ${multimedia_video[@]}; do
+        videos=$(find . -type f -name "*.$iterator" | wc -l)
+        counter_videos=$(($counter_videos + $videos))
+    done
+    echo "Total video: $counter_videos"
+    echo "Total files to process: $(($counter_pictures + $counter_videos))"
+
+    counter=0
+    # Compress the files
+    find . -type f | while read file; do
+        extension="${file##*.}"
+        if [[ "${multimedia_video[*]}" =~ "${extension,,}" ]]; then
+            counter=$(($counter + 1))
+            echo -e "'\033[0;34m'Processing file $counter / $(($counter_pictures + $counter_videos)) -- $(($counter * 100 / $(($counter_pictures + $counter_videos))))% '\e[0m'"
+            echo "Compressing video: $file"
+            HandBrakeCLI -e x265 --x265-preset medium -q 25 --crop 0:0:0:0 --aencoder opus -f av_mkv -i "$file" -o "$file.mkv"
+            if [[ $? == 0 ]]; then
+                echo "No errors reported: deleting original"
+                rm "$file"
+            else
+                echo "Exit code is not 0: deleting artifact"
+                rm "$file.mkv"
+            fi
+        fi
+        if [[ "${multimedia_images[*]}" =~ "${extension,,}" ]]; then
+            counter=$(($counter + 1))
+            echo -e "'\033[0;34m'Processing file $counter / $(($counter_pictures + $counter_videos)) -- $(($counter * 100 / $(($counter_pictures + $counter_videos))))% '\e[0m'"
+            echo "Compressing image: $file"
+            ffmpeg -i "$file" -crf 25 "$file.avif"
+            if [[ $? == 0 ]]; then
+                echo "No errors reported: deleting original"
+                rm "$file"
+            else
+                echo "Exit code is not 0: deleting artifact"
+                rm "$file.avif"
+            fi
+        fi
+        if [[ "${unstable_multimedia_images[*]}" =~ "${extension,,}" ]]; then
+            counter=$(($counter + 1))
+            echo -e "'\033[0;34m'Processing file $counter / $(($counter_pictures + $counter_videos)) -- $(($counter * 100 / $(($counter_pictures + $counter_videos))))% '\e[0m'"
+            echo "Compressing image: $file"
+            ffmpeg -i "$file" -crf 0 "$file.jpg" # I honestly doubt that -crf 0 work here and makes it lossless.
+            ffmpeg -i "$file" -crf 25 "$file.avif"
+            if [[ $? == 0 ]]; then
+                echo "No errors reported: deleting original and temporary artifact"
+                rm "$file" "$file.jpg"
+            else
+                echo "Exit code is not 0: deleting artifact"
+                rm "$file.avif" "$file.jpg"
+            fi
+        fi
+    done
+
+    popd
+}
+
+
+main () {
+    echo "Arkivr v1"
+    case $1 in
+        "--help")
+            help_message
+            ;;
+        "--formats")
+            supported_formats
+            ;;
+        "--compress")
+            compress "$2"
+            ;;
+    esac
+}
+
+main $1 "$2"
